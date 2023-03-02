19826e451919de58997f8e6a3653d41ec394b91a
a895576 <a895576@DESKTOP-SQVFD1M>
2023-03-02 18:56:39 +0530

    first commit

---
+++ DsskafkaRestAdapter.Application/Constants/KafkaTopics.cs
@@ -0,0 +1,7 @@
+﻿namespace DsskafkaRestAdapter.Application.Constants
+{
+    public static class KafkaTopics
+    {
+        public static string AddDocumentRequest => "AddDocumentRequest";
+    }
+}
---
+++ DsskafkaRestAdapter/MessageConsumer.cs
@@ -0,0 +1,42 @@
+﻿using Confluent.Kafka;
+using DsskafkaRestAdapter.Application.Constants;
+using Kafka.Interfaces;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsskafkaRestAdapter
+{
+    public class MessageConsumer : BackgroundService
+    {
+        private readonly IKafkaConsumer<string, AddDocument> _consumer;
+        public MessageConsumer(IKafkaConsumer<string, AddDocument> kafkaConsumer)
+        {
+            _consumer = kafkaConsumer;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                await _consumer.Consume(KafkaTopics.AddDocumentRequest, stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{(int)HttpStatusCode.InternalServerError} ConsumeFailedOnTopic - {KafkaTopics.AddDocumentRequest}, {ex}");
+            }
+        }
+
+        public override void Dispose()
+        {
+            _consumer.Close();
+            _consumer.Dispose();
+
+            base.Dispose();
+        }
+    }
+}
